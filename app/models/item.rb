9bd760aa7a9807da0c9a5ef892de33fadc37299b
Boris Goryachev <jesteracer@me.com>
2013-09-27 13:57:13 +0400

    Item model refactoring

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true, numericality: true
+  validates_presence_of :name, :description, :price
+  validates :price, numericality: {greater_than: 0}
 end
